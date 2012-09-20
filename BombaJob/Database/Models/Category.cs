c7e72984fffac0908c1beb78b6b65f4ad5e751a6
supudo <supudo@gmail.com>
2012-09-20 17:08:27 +0300

    rev

---
+++ BombaJob/Database/Models/Category.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace BombaJob.Database.Models
+{
+    public class Category
+    {
+        private int _categoryID;
+        public int CategoryID
+        {
+            get
+            {
+                return this._categoryID;
+            }
+            set
+            {
+                this._categoryID = value;
+            }
+        }
+
+        private int _offersCount;
+        public int OffersCount
+        {
+            get
+            {
+                return this._offersCount;
+            }
+            set
+            {
+                this._offersCount = value;
+            }
+        }
+
+        private string _title;
+        public string Title
+        {
+            get
+            {
+                return this._title;
+            }
+            set
+            {
+                this._title = value;
+            }
+        }
+    }
+}
