7074e0b1c5ba4cc0f32d66077e343f102bd8b842
supudo <supudo@gmail.com>
2012-09-20 17:07:54 +0300

    rev

---
+++ BombaJob/Utilities/OfferIconConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace BombaJob.Utilities
+{
+    public class OfferIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return null;
+            return "../images/" + value.ToString() + ".png";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();    
+        }
+    }
+}
