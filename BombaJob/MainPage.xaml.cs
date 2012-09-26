3d94a6f73b4903c106ab2a5f02ebd3b758c22ece
supudo <supudo@gmail.com>
2012-09-26 14:15:46 +0300

    rev

--- BombaJob/MainPage.xaml.cs
+++ BombaJob/MainPage.xaml.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Net;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -18,44 +17,14 @@ namespace BombaJob
 {
     public partial class MainPage : BombaJobBasePage
     {
-        private Popup popup;
-        private SplashScreen splashScreen;
-
         public MainPage()
         {
             InitializeComponent();
-            ShowPopup();
+            base.SyncComplete += new BombaJobBasePage.EventHandler(MainPage_SyncComplete);
+            base.DoSync();
         }
 
-        private void ShowPopup()
-        {
-            if (this.splashScreen == null)
-                this.splashScreen = new SplashScreen();
-            this.splashScreen.SplashError += new SplashScreen.EventHandler(splashScreen_SplashError);
-            this.splashScreen.SplashComplete += new SplashScreen.EventHandler(splashScreen_SplashComplete);
-
-            this.popup = new Popup();
-            this.popup.Child = this.splashScreen;
-            this.popup.IsOpen = true;
-            this.splashScreen.startSync();
-        }
-
-        void splashScreen_SplashComplete(object sender, BombaJobEventArgs e)
-        {
-            this.Dispatcher.BeginInvoke(() =>
-            {
-                this.popup.IsOpen = false;
-            });
-            RenderNewest();
-        }
-
-        void splashScreen_SplashError(object sender, BombaJobEventArgs e)
-        {
-            if (e.IsError)
-                MessageBox.Show(e.ErrorMessage);
-        }
-
-        void RenderNewest()
+        void MainPage_SyncComplete(object sender, BombaJobEventArgs e)
         {
             base.BuildApplicationBar();
             NavigationService.Navigate(new Uri("/Views/Newest.xaml", UriKind.Relative));
--- BombaJob/Utilities/BombaJobBasePage.cs
+++ BombaJob/Utilities/BombaJobBasePage.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Documents;
 using System.Windows.Ink;
 using System.Windows.Input;
@@ -15,9 +16,62 @@ namespace BombaJob.Utilities
 {
     public class BombaJobBasePage : PhoneApplicationPage
     {
+        public delegate void EventHandler(Object sender, BombaJobEventArgs e);
+        public event EventHandler SyncComplete;
+        public event EventHandler SyncError;
+
+        private Popup popup;
+        private SplashScreen splashScreen;
+
         public BombaJobBasePage()
         {
             SupportedOrientations = SupportedPageOrientation.Portrait | SupportedPageOrientation.Landscape;
+        }
+
+        public void DoSync()
+        {
+            if (this.ApplicationBar != null)
+                this.ApplicationBar.IsVisible = false;
+            this.ShowSyncPopup();
+        }
+
+        private void ShowSyncPopup()
+        {
+            if (this.splashScreen == null)
+                this.splashScreen = new SplashScreen();
+            this.splashScreen.SplashError += new SplashScreen.EventHandler(splashScreen_SplashError);
+            this.splashScreen.SplashComplete += new SplashScreen.EventHandler(splashScreen_SplashComplete);
+
+            this.popup = new Popup();
+            this.popup.Child = this.splashScreen;
+            this.popup.IsOpen = true;
+            this.splashScreen.startSync();
+        }
+
+        void splashScreen_SplashComplete(object sender, BombaJobEventArgs e)
+        {
+            this.Dispatcher.BeginInvoke(() =>
+            {
+                this.popup.IsOpen = false;
+                if (this.ApplicationBar != null)
+                    this.ApplicationBar.IsVisible = true;
+                try
+                {
+                    SyncComplete(this, new BombaJobEventArgs(e.IsError, "", ""));
+                }
+                catch { }
+            });
+        }
+
+        void splashScreen_SplashError(object sender, BombaJobEventArgs e)
+        {
+            if (e.IsError)
+                MessageBox.Show(e.ErrorMessage);
+            this.ApplicationBar.IsVisible = true;
+            this.Dispatcher.BeginInvoke(() =>
+            {
+                SyncError(this, new BombaJobEventArgs(e.IsError, e.ErrorMessage, ""));
+            });
         }
 
         public void BuildApplicationBar()
@@ -26,38 +80,38 @@ namespace BombaJob.Utilities
 
             ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("images/menu/tb-newest.png", UriKind.Relative));
             appBarButton.Text = AppResources.menu_Newest;
-            appBarButton.Click += new EventHandler(menuNewest_Click);
+            appBarButton.Click += new System.EventHandler(menuNewest_Click);
             this.ApplicationBar.Buttons.Add(appBarButton);
 
             appBarButton = new ApplicationBarIconButton(new Uri("images/menu/tb-jobs.png", UriKind.Relative));
             appBarButton.Text = AppResources.menu_Jobs;
-            appBarButton.Click += new EventHandler(menuJobsClick);
+            appBarButton.Click += new System.EventHandler(menuJobsClick);
             this.ApplicationBar.Buttons.Add(appBarButton);
 
             appBarButton = new ApplicationBarIconButton(new Uri("images/menu/tb-people.png", UriKind.Relative));
             appBarButton.Text = AppResources.menu_People;
-            appBarButton.Click += new EventHandler(menuPeople_Click);
+            appBarButton.Click += new System.EventHandler(menuPeople_Click);
             this.ApplicationBar.Buttons.Add(appBarButton);
 
             appBarButton = new ApplicationBarIconButton(new Uri("images/menu/tb-search.png", UriKind.Relative));
             appBarButton.Text = AppResources.menu_Search;
-            appBarButton.Click += new EventHandler(menuSearch_Click);
+            appBarButton.Click += new System.EventHandler(menuSearch_Click);
             this.ApplicationBar.Buttons.Add(appBarButton);
 
             ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.menu_Post);
-            appBarMenuItem.Click += new EventHandler(menuPost_Click);
+            appBarMenuItem.Click += new System.EventHandler(menuPost_Click);
             this.ApplicationBar.MenuItems.Add(appBarMenuItem);
 
             appBarMenuItem = new ApplicationBarMenuItem(AppResources.menu_Settings);
-            appBarMenuItem.Click += new EventHandler(menuSettings_Click);
+            appBarMenuItem.Click += new System.EventHandler(menuSettings_Click);
             this.ApplicationBar.MenuItems.Add(appBarMenuItem);
 
             appBarMenuItem = new ApplicationBarMenuItem(AppResources.menu_Sync);
-            appBarMenuItem.Click += new EventHandler(menuSync_Click);
+            appBarMenuItem.Click += new System.EventHandler(menuSync_Click);
             this.ApplicationBar.MenuItems.Add(appBarMenuItem);
 
             appBarMenuItem = new ApplicationBarMenuItem(AppResources.menu_About);
-            appBarMenuItem.Click += new EventHandler(menuAbout_Click);
+            appBarMenuItem.Click += new System.EventHandler(menuAbout_Click);
             this.ApplicationBar.MenuItems.Add(appBarMenuItem);
         }
 
@@ -92,6 +146,7 @@ namespace BombaJob.Utilities
 
         void menuSync_Click(object sender, EventArgs e)
         {
+            this.DoSync();
         }
 
         void menuAbout_Click(object sender, EventArgs e)
